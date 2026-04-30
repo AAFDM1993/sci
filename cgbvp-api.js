f927166e93ec0865a7b5d0b407eba4e649a55d69
AAFDM1993 <alex.fernandezdavila.m@gmail.com>
2026-04-29 22:12:15 -0500

    Update cgbvp-api.js

--- cgbvp-api.js
+++ cgbvp-api.js
@@ -8,7 +8,7 @@ const CGBVP_API = (() => {
 
   // ── Configuración ─────────────────────────────────────────
   // Reemplaza con tu URL de despliegue de GAS
-  const API_URL = 'https://script.google.com/macros/s/AKfycbzfVbtka_7nMtOegbknt-NiBQLB41Q7E9kGUFObkIWsK0wzyrFkuZge13QVt8_c7Jb0Ng/exec';
+  const API_URL = 'https://script.google.com/macros/s/AKfycbzt0l53i4QZNpi7vXCrYSC_3T13tjhxJ9UEq60PhCR9QRFLZ3qPFZrQvT1-Fy36zRq6nw/exec';
 
   const STORAGE_KEYS = {
     TOKEN   : 'cgbvp_token',
